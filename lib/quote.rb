edfcbc1047d4d5ceeb9131161b39700defc2fa42
Jabair <jabairk@gmail.com>
2021-03-09 13:21:31 -0500

    Added association macros, started making methods

--- lib/quote.rb
+++ lib/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+    has_many :user_fortunes 
+    has_many :users, through: :user_fortunes 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+    has_many :user_fortunes 
+    has_many :quotes, through: :user_fortunes 
+    
+
+    def get_all_fortunes
+        self.quotes.each do |quote|
+            put quote.all 
+        end
+        
+
+    end
+
+#     binding.pry 
 end
